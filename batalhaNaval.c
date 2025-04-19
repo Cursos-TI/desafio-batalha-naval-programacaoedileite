2b739c5a7b95ee4a2ea604da58f521c067f3ed93
programacaoedileite <edileusaleite42ads@gmail.com>
2025-04-19 17:46:50 +0000

    commit tabuleiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,6 +4,42 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
+// Função para imprimir o Tabuleiro
+void imprimirTabuleiro(int tabuleiro[10][10]){
+    char linha1[10]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+
+    for(int i = 1; i <= 10; i++){
+        printf("%2d ", i); // Imprime número das colunas
+    }
+    printf("\n");
+
+    for(int i = 0; i < 10; i++){
+        printf("%c ", linha1[i]); // Imprime letra das linhas
+
+        for(int j = 0; j < 10; j++){
+            printf("%2d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Função para posicionar um navio
+int posicionarNavio(int tabuleiro[10][10], int linha, int coluna, int tamanho, int orientacao){
+    
+    for(int i = 0; i < tamanho; i++){
+        int linhaAtual = linha;
+        int colunaAtual = coluna;
+
+        if(orientacao == 1){
+            linhaAtual += i; // Faz atribuição com soma na linha
+        }
+        else {
+            colunaAtual += i;
+        }
+        tabuleiro[linhaAtual][colunaAtual] = 3; // 1- navio
+    }
+}
+
 int main() {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
@@ -36,5 +72,21 @@ int main() {
     // 1 1 1 1 1
     // 0 0 1 0 0
 
+    int tabuleiro[10][10] = {0}; // Inicializa o tabuleiro com 0(vazio)
+
+    // Exibindo o Tabuleiro
+    printf("\n *** TABULEIRO BATALHA NAVAL *** \n\n");
+    printf("  ");
+
+    // Posicionando um navio de tamanho 3 horizontalmente na linha 2, coluna 2
+    if(posicionarNavio(tabuleiro, 2, 2, 3, 0))
+
+    // Posicionando um navio de tamanho 3 verticalmente na linha 5, coluna 1
+    if(posicionarNavio(tabuleiro, 5, 1, 3, 1))
+
+    imprimirTabuleiro(tabuleiro);
+
+    printf("\n");
+    
     return 0;
 }
